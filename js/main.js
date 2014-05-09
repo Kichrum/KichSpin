7d1f962a3fffb3343024bae61c02023c81b441d5
Sergii Pashko <kichrum@kichrum.org.ua>
2014-05-09 19:23:42 +0300

    Random spin

--- js/main.js
+++ js/main.js
@@ -3,5 +3,26 @@
 */
 
 $('document').ready(function(){
-    $('.col').append($('.symbols').clone());
+    fillColumns();
+    $('.button').on('click', spin);
 });
+
+function shuffle(o){
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
+
+var fillColumns = function() {
+    $('.col').html('');
+    $('.col').each(function(j, col) {
+        var symbols = shuffle([1,2,3,4,5]);
+        for(var i = 0; i < 10; i++) {
+            $(col).append($('.symbols .symbol' + symbols[i%5]).clone());
+        }
+    });
+};
+
+var spin = function(e) {
+    e.preventDefault();
+    fillColumns();
+};
