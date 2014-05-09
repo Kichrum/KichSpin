e6c8d9b02e5d23d51b3bacbce899d88fae11e7c6
Sergii Pashko <kichrum@kichrum.org.ua>
2014-05-09 18:39:19 +0300

    Initial CSS styles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*
+    Author     : kichrum
+*/
 
-
+$('document').ready(function(){
+    $('.col').append($('.symbols').clone());
+});
