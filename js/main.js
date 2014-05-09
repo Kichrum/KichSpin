c79f8d2eed3a3027e2bb46027c582eee152c451a
Sergii Pashko <kichrum@kichrum.org.ua>
2014-05-09 20:14:58 +0300

    Basic animation

--- js/main.js
+++ js/main.js
@@ -12,13 +12,24 @@ function shuffle(o){
     return o;
 };
 
+var cleanColumn = function($col) {
+    $col.find('.symbol').each(function(i, symbol) {
+        if (i > 2) {
+            $(symbol).remove();
+        }
+    });
+};
+
 var fillColumns = function() {
-    $('.col').html('');
     $('.col').each(function(j, col) {
+        cleanColumn($(col));
+//        $(col).css('margin-top', '-1227px').css('height', '1228');
+        $(col).css('margin-top', '-1750px').css('height', '1751');
         var symbols = shuffle([1,2,3,4,5]);
         for(var i = 0; i < 10; i++) {
-            $(col).append($('.symbols .symbol' + symbols[i%5]).clone());
+            $(col).prepend($('.symbols .symbol' + symbols[i%5]).clone());
         }
+        $(col).animate({ marginTop: '0' }, 1000);
     });
 };
 
